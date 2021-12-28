fad41da8717190fc5baeac1cf6c4f47df0f473c8
Ducanh2404 <1951060533@e.tlu.edu.vn>
2021-12-28 21:10:33 +0700

    new

--- js/script.js
+++ js/script.js
@@ -3,8 +3,8 @@ let add = document.getElementById('add')
 let phone = document.getElementById('phone')
 let check=document.getElementById('vn')
 let tour = document.getElementById('tour')
-let maybay= document.getElementById('rd1').checked
-let oto= document.getElementById('rd2').checked
+let maybay= document.getElementById('rd1').checked;
+let oto= document.getElementById('rd2').checked;
 let adult= document.getElementById('adult')
 let kid= document.getElementById('kid')
 let note= document.getElementById('note')
@@ -12,6 +12,6 @@ let btnConfirm = document.getElementById('btnConfirm')
 let btnReset = document.getElementById('btnReset')
 
 btnConfirm.addEventListener('click',function(){
-    console.log
+    console.log(maybay)
     // if(nm.value=="" || add.value=="" || phone.value=="" || adult.value=="" || kid.value=="" || maybay.checked=false || oto.checked=false )
 })
