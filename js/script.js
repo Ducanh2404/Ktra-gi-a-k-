cbefe8fb95aeedf5dc551bc6c564d7d851870dac
Ducanh2404 <1951060533@e.tlu.edu.vn>
2021-12-28 21:00:14 +0700

    new

---
+++ js/script.js
@@ -0,0 +1,17 @@
+let nm = document.getElementById('name')
+let add = document.getElementById('add')
+let phone = document.getElementById('phone')
+let check=document.getElementById('vn')
+let tour = document.getElementById('tour')
+let maybay= document.getElementById('rd1').checked
+let oto= document.getElementById('rd2').checked
+let adult= document.getElementById('adult')
+let kid= document.getElementById('kid')
+let note= document.getElementById('note')
+let btnConfirm = document.getElementById('btnConfirm')
+let btnReset = document.getElementById('btnReset')
+
+btnConfirm.addEventListener('click',function(){
+    console.log
+    // if(nm.value=="" || add.value=="" || phone.value=="" || adult.value=="" || kid.value=="" || maybay.checked=false || oto.checked=false )
+})
